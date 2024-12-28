b6cc89babe58e489993023083a1db8d6d70468d9
Yanjiaqi0328 <yanjiaqi0328@gmail.com>
2024-12-28 17:56:09 +0800

    Update H_control_tank.py

--- H_control_tank.py
+++ H_control_tank.py
@@ -6,6 +6,7 @@ import scipy.linalg
 from cvxopt import solvers, matrix
 import copy
 import cvxpy as cp
+from utils import get_trajectory, evaluate_model
 
 seed = 3
 np.random.seed(seed + 123)
@@ -37,88 +38,16 @@ C_ = np.array(C_)*15
 A = np.hstack([np.vstack([A_, C_ @ A_]), np.zeros([dim_z + dim_y, dim_y])])
 B = np.vstack([B_, C_ @ B_])
 
-def cal_matrices(x_kshort, ref_tra):
-    Q = np.eye(dim_z + dim_y)
-    Q[:dim_z, :] = 0
-    Q[dim_z + 2:, :] = 0
-    F = np.eye(dim_z + dim_y)
-    R = np.eye(dim_u) * 0.01
-
-    n = A.shape[0]
-    p = B.shape[1]
-
-    M = np.vstack((np.eye((n)), np.zeros((N*n,n))))
-    C = np.zeros(((N+1)*n,N*p))
-    tmp = np.eye(n)
-
-    for i in range(N):
-        rows = i * n + n
-        C[rows:rows+n,:] = np.hstack((np.dot(tmp, B), C[rows-n:rows, 0:(N-1)*p]))
-        tmp = np.dot(A, tmp)
-        M[rows:rows+n,:] = tmp
-
-    Q_bar_be = np.kron(np.eye(N), Q)
-    Q_bar = scipy.linalg.block_diag(Q_bar_be, F)
-    R_bar = np.kron(np.eye(N), R)
-
-    E = np.matmul(np.matmul(C.transpose(),Q_bar),M)
-    L = - np.matmul(C.transpose(), Q_bar)
-    H = np.matmul(np.matmul(C.transpose(),Q_bar),C) + R_bar
-    M = H
-    C = E
-    M = matrix(M)
-
-    ref = np.vstack([np.zeros([dim_z, N]), ref_tra])
-    ref = ref.transpose().reshape([-1, 1])
-    ref = np.vstack([x_kshort, ref])
-    T = np.dot(C, x_kshort) + np.dot(L, ref)
-    T = matrix(T)
-    G = matrix(np.vstack([np.eye(dim_u * N), - np.eye(dim_u * N)]))
-    H = matrix(np.ones([dim_u * N * 2, 1]) * 10000)
-    try:
-        sol = solvers.qp(M, T, G, H, kktsolver='ldl', options={'show_progress': False})
-    except:
-        u_k = np.random.randn(dim_u)
-    else:
-        U_thk = np.array(sol["x"])
-        u_k = U_thk[:dim_u, :]
-    return u_k
-
-def get_trajectory(length):
-    z = np.random.randn(dim_z, 1)
-    trajectory = np.zeros([dim_u + dim_y, length])
-    for i in range(length):
-        u = np.random.randn(dim_u, 1)
-        z = A_ @ z + B_ @ u
-        y = C_ @ z
-        trajectory[: dim_u, i] = u.reshape(-1)
-        trajectory[dim_u:, i] = y.reshape(-1)
-    return trajectory
-
-def evaluate_model():
-    z = np.zeros([dim_z, 1])
-    u = np.zeros([dim_u, 1])
-    y = np.zeros([dim_y, 1])
-    trajectory = np.zeros([dim_u + dim_y, len(ref_theta)])
-    for i in range(1, num_points):
-        s = np.vstack([z, y]).reshape([-1, 1])
-        u = cal_matrices(s, ref_trajectory[:, i: i + N])
-        z = A_ @ z + B_ @ u
-        y = C_ @ z
-        trajectory[: dim_u, i] = u.reshape(-1)
-        trajectory[dim_u:, i] = y.reshape(-1)
-    return trajectory
-
 L = 5
 num_T = (dim_u + 1) * L + dim_z - 1
-base_trajectory = get_trajectory(num_T)
+base_trajectory = get_trajectory(num_T, dim_z, dim_u, dim_y, A_, B_, C_)
 wd = base_trajectory.transpose().reshape([-1, 1])
 H = np.zeros([L * (dim_u + dim_y), num_T - L + 1])
 for i in range(num_T - L + 1):
     H[:, i] = wd[i * (dim_u + dim_y) : (i + L) * (dim_u + dim_y)].reshape(-1)
 
 attack_level = 1.
-trajectory = evaluate_model()
+trajectory = evaluate_model(dim_z, dim_u, dim_y, ref_theta, num_points, ref_trajectory, N, A, B, A_, B_, C_)
 noise_trajectory = np.copy(trajectory)
 #noise_trajectory += noise_level * np.random.randn(noise_trajectory.shape[0], noise_trajectory.shape[1]) # Gaussian noise
 noise_trajectory += np.random.uniform(-0.1, 0.1, size=(noise_trajectory.shape[0], noise_trajectory.shape[1])) # Uniform noise
